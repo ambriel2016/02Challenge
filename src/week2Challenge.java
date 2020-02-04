36d9ed4daf6b28e4b4cbd227001c4bb98fde75e3
GBTC440002UR <GBTC440002UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-04 16:32:52 -0500

    Week 2 Challenge Corrected

--- src/week2Challenge.java
+++ src/week2Challenge.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 
-public class weekTwoChallenge {
-    public static void main(String[]) {
+public class week2Challenge {
+    public static void main(String[] args) {
 
         // variables
         int counter = 1;
@@ -10,17 +10,18 @@ public class weekTwoChallenge {
         // do while loop until the counter equals to 25 burritos
         do {
             ArrayList<String> burrito = new ArrayList<String>();
-            burrito.add("riceChoice");
-            burrito.add("meatChoice");
-            burrito.add("beansChoice");
-            burrito.add("salsaChoice");
-            burrito.add("veggiesChoice");
-            burrito.add("cheeseChoice");
-            burrito.add("guacChoice");
-            burrito.add("quesoChoice");
-            burrito.add("sourCream");
+            //This in here should be the data type including empty ()
+            burrito.add(riceChoice());
+            burrito.add(meatChoice());
+            burrito.add(beansChoice());
+            burrito.add(salsaChoice());
+            burrito.add(veggiesChoice());
+            burrito.add(cheeseChoice());
+            burrito.add(guacChoice());
+            burrito.add(quesoChoice());
+            burrito.add(sourCream());
 
-            System.out.print("Burrito" + counter + ": ");
+            System.out.print("Burrito " + counter + ": ");
             for (int b = 0; b < burrito.size(); b++) {
                 if (burrito.get(b).contains("all")) {
                     ingCounter += 3;
@@ -34,14 +35,17 @@ public class weekTwoChallenge {
                 }
             }
             cost(counter, ingCounter);
-
+            //this one should counter from 0 in order to get the correct price of the Burrito
+            ingCounter = 0;
             System.out.println();
 
             burrito.clear();
 
             counter++;
 
-        }while(counter <= 25);
+        } while (counter <= 25);
+    }
+    // the public methods should be outside of the main method
 
         public static void cost(int count, int ingred){
             System.out.println("\nBurrito " + count + " cost: $" + (3 +(.50*ingred)) + "0");
@@ -53,7 +57,7 @@ public class weekTwoChallenge {
             rice.add("brown");
             rice.add("none");
             rice.add("all");
-            return rice.get((int)) (Math.random()* rice.size()));
+            return rice.get((int) (Math.random()* rice.size()));
         }
 
         public static String meatChoice(){
@@ -65,7 +69,7 @@ public class weekTwoChallenge {
             meat.add("sofritas");
             meat.add("none");
             meat.add("all");
-            return meat.get((int)) (Math.random()* meat.size()));
+            return meat.get((int) (Math.random()* meat.size()));
         }
 
         public static String beansChoice(){
@@ -74,7 +78,7 @@ public class weekTwoChallenge {
             beans.add("black");
             beans.add("none");
             beans.add("all");
-            return beans.get((int)) (Math.random()* beans.size()));
+            return beans.get((int) (Math.random()* beans.size()));
         }
 
         public static String salsaChoice(){
@@ -84,7 +88,7 @@ public class weekTwoChallenge {
             salsa.add("hot");
             salsa.add("none");
             salsa.add("all");
-            return salsa.get((int)) (Math.random()* salsa.size()));
+            return salsa.get((int) (Math.random()* salsa.size()));
         }
 
         public static String veggiesChoice(){
@@ -93,36 +97,35 @@ public class weekTwoChallenge {
             veggies.add("fajita veggies");
             veggies.add("none");
             veggies.add("all");
-            return veggies.get((int)) (Math.random()* veggies.size()));
+            return veggies.get((int) (Math.random()* veggies.size()));
         }
 
         public static String cheeseChoice(){
             ArrayList<String> cheese = new ArrayList<>();
             cheese.add("cheese");
             cheese.add("no cheese");
-            return cheese.get((int)) (Math.random()* cheese.size()));
+            return cheese.get((int) (Math.random()* cheese.size()));
         }
 
         public static String guacChoice(){
             ArrayList<String> guac = new ArrayList<>();
             guac.add("guac");
             guac.add("no guac");
-            return guac.get((int)) (Math.random()* guac.size()));
+            return guac.get((int) (Math.random()* guac.size()));
         }
 
         public static String quesoChoice(){
             ArrayList<String> queso = new ArrayList<>();
             queso.add("queso");
             queso.add("no queso");
-            return queso.get((int)) (Math.random()* queso.size()));
+            return queso.get((int) (Math.random()* queso.size()));
         }
 
         public static String sourCream(){
             ArrayList<String> sour = new ArrayList<>();
             sour.add("sour cream");
             sour.add("no sour cream");
-            return sour.get((int)) (Math.random()* sour.size()));
+            return sour.get((int) (Math.random()* sour.size()));
         }
 
-    }
 }
